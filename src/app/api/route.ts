af96ffa083333122d74ecc5b5420f915b3f58932
sohamviradiya <sohamviradiya2003@gmail.com>
2023-07-24 22:34:22 +0530

    add base api

---
+++ src/app/api/route.ts
@@ -0,0 +1,14 @@
+import { STATUS_CODES } from '@/server/response/response.module';
+import { returnResponse } from '@/server/response/response.util';
+import { NextRequest } from 'next/server';
+
+export async function GET() {
+    return returnResponse({ code: STATUS_CODES.OK, message: "Welcome to Nyarlathotep" });
+};
+
+export async function POST(request: NextRequest) {
+    let { name } = await request.json();
+    if (!name)
+        name = "Anonymous";
+    return returnResponse({ code: STATUS_CODES.OK, message: `Welcome to Nyarlathotep ${name}` });
+}
