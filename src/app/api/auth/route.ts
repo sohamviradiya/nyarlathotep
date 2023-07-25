d424ff8bb8a5447e940db1ffd34b029fed66f155
sohamviradiya <sohamviradiya2003@gmail.com>
2023-07-25 16:52:40 +0530

    add firebase error handlers

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,50 +1,49 @@
-import { addCredentials, deleteCredentials, generateClientToken, getUserIDFromToken, updateCredentials } from '@/server/auth/auth.service';
+import { generateClientToken, verifyClientToken, updateCredentials } from '@/server/auth/auth.service';
 import { extractToken } from '@/server/auth/auth.util';
 import { STATUS_CODES } from '@/server/response/response.module';
-import { returnResponse } from '@/server/response/response.util';
+import { returnError, returnResponse } from '@/server/response/response.util';
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
     const token = extractToken(request.headers);
-    if (!token) return returnResponse({ code: STATUS_CODES.BAD_REQUEST, message: 'Missing token' });
-    return returnResponse(await getUserIDFromToken(token));
+    if (!token) return returnError({ message: 'Missing token' });
+    try {
+        return returnResponse(await verifyClientToken(token));
+    }
+    catch (error: any) {
+        return returnError(error);
+    }
 };
 
 export async function POST(request: NextRequest) {
     const { auth } = await request.json();
-    if (!auth.email || !auth.password) return returnResponse({ code: STATUS_CODES.BAD_REQUEST, message: 'Missing email or password' });
-    return returnResponse(
-        await addCredentials({
-            email: auth.email,
-            password: auth.password,
-        })
-    );
-}
+    if (!auth.email || !auth.password) return returnError({ message: 'Missing credentials' });
+    try {
+        return returnResponse(
+            await generateClientToken({
+                email: auth.email,
+                password: auth.password,
+            })
+        );
+    }
+    catch (error: any) {
+        return returnError(error);
+    }
+};
 
 export async function PUT(request: NextRequest) {
     const { auth } = await request.json();
-    if (!auth.email || !auth.password) return returnResponse({ code: STATUS_CODES.BAD_REQUEST, message: 'Missing email or password' });
-    return returnResponse(
-        await generateClientToken({
-            email: auth.email,
-            password: auth.password,
-        }) 
-    );
-};
-
-export async function PATCH(request: NextRequest) {
-    const { auth } = await request.json();
-    if (!auth.email || !auth.currentPassword || !auth.newPassword) return returnResponse({ code: STATUS_CODES.BAD_REQUEST, message: 'Missing email or password' });
-    return returnResponse(
-        await updateCredentials({
-            email: auth.email,
-            currentPassword: auth.currentPassword,
-            newPassword: auth.newPassword,
-        })
-    );
-}
-export async function DELETE(request: NextRequest) {
-    const token = extractToken(request.headers);
-    if (!token) return returnResponse({ code: STATUS_CODES.BAD_REQUEST, message: 'Missing token' });
-    return returnResponse(await deleteCredentials(token));
+    if (!auth.email || !auth.currentPassword || !auth.newPassword) return returnError({ message: 'Missing credentials' });
+    try {
+        return returnResponse(
+            await updateCredentials({
+                email: auth.email,
+                currentPassword: auth.currentPassword,
+                newPassword: auth.newPassword,
+            })
+        );
+    }
+    catch (error: any) {
+        return returnError(error);
+    }
 }
