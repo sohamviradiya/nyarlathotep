9bddcbd42d7cc499a4dcc8327421e9617b51cd20
sohamviradiya <sohamviradiya2003@gmail.com>
2023-07-25 16:53:19 +0530

    add user search api

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,13 +1,18 @@
 import { STATUS_CODES } from '@/server/response/response.module';
-import { returnResponse } from '@/server/response/response.util';
+import { returnError, returnResponse } from '@/server/response/response.util';
 import { searchUsersByName } from '@/server/user/user.service';
 import { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-    const search_string = request.nextUrl.searchParams.get('query_string');
-    if (!search_string) return returnResponse({ code: STATUS_CODES.BAD_REQUEST, message: 'No query string provided' });
+    const search_string = request.nextUrl.searchParams.get('query');
+    if (!search_string) return returnError({ message: 'Missing query string' });
     const limit = parseInt(request.nextUrl.searchParams.get('limit') || '20');
-    return returnResponse(await searchUsersByName(search_string, limit));
+    try {
+        return returnResponse(await searchUsersByName(search_string, limit));
+    }
+    catch (error: any) {
+        return returnError({ message: error.message });
+    }
 };
 
 
