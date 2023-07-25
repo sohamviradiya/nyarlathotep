c085f9077e237f6bcbc7c869635b04e422981847
sohamviradiya <sohamviradiya2003@gmail.com>
2023-07-25 18:03:28 +0530

    update response methods

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -2,30 +2,32 @@ import { extractToken } from "@/server/auth/auth.util";
 import { REQUEST_TYPE } from "@/server/request/request.module";
 import { sendRequest } from "@/server/request/request.service";
 import { STATUS_CODES } from "@/server/response/response.module";
-import { returnError, returnResponse } from "@/server/response/response.util";
+import { BadReq, ApiResponse, Unauthorized } from "@/server/response/response.util";
 import { getUserByID } from "@/server/user/user.service";
 import { NextRequest } from "next/server"
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
     try {
-        return returnResponse(await getUserByID(params.id));
+        return ApiResponse(await getUserByID(params.id));
     }
     catch (error: any) {
-        return returnError(error);
+        return BadReq(error);
     }
 };
 
 export async function POST(request: NextRequest, { params }: { params: { id: string } }) {
     const token = extractToken(request.headers);
-    if (!token)
-        return returnResponse({ code: STATUS_CODES.UNAUTHORIZED, message: "Unauthorized, Please Login again" });
-
+    if (!token) return Unauthorized({ message: "No token provided" });
     const body = await request.json();
-
-    return returnResponse(await sendRequest({
-        receiver: params.id,
-        type: REQUEST_TYPE.CONNECT,
-        message: body.message || "",
-    }, token));
+    try {
+        return ApiResponse(await sendRequest({
+            receiver: params.id,
+            type: REQUEST_TYPE.CONNECT,
+            message: body.message || "",
+        }, token));
+    }
+    catch (error: any) {
+        return BadReq(error);
+    }
 }
 
