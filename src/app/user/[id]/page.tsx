e02618b5e79b1a2b4e3e3f0737dadafb04784941
sohamviradiya <sohamviradiya2003@gmail.com>
2023-07-27 15:31:43 +0530

    add main page

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,13 +1,12 @@
 import styles from "@/app/page.module.css";
 import { UserSearch } from "@/components/usersearch.main";
 
-export default function Home() {
+export default function User({ params }: { params: { id: string } }) {
     return (
         <main className={styles.main}>
-            <h1 style={{backgroundColor: "darkblue", padding: "2rem"}}>
-                Welcome To NyarlaThotep 
+            <h1 style={{ backgroundColor: "darkblue", padding: "2rem" }}>
+                {decodeURIComponent(params.id)}
             </h1>
-            <UserSearch />
         </main>
     )
 }
