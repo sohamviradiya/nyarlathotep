2a4def59934693184086290f053209e03392dfa6
sohamviradiya <sohamviradiya2003@gmail.com>
2023-08-04 18:38:37 +0530

    add message info api

--- src/app/api/message/[id]/route.ts
+++ src/app/api/message/[id]/route.ts
@@ -1,7 +1,18 @@
 import { extractToken } from "@/server/auth/auth.util";
-import { confirmMessage, deleteMessage, updateMessage } from "@/server/message/message.service";
+import { confirmMessage, deleteMessage, getMessage, updateMessage } from "@/server/message/message.service";
 import { BadReq, ApiResponse, Unauthorized } from "@/server/response/response.util";
 import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+    const token = extractToken(request.headers);
+    if (!token) return ApiResponse(Unauthorized({ message: "Missing token" }));
+    try {
+        return ApiResponse(await getMessage(params.id, token));
+    }
+    catch (error: any) {
+        return ApiResponse(BadReq(error));
+    }
+};
 
 export async function POST(request: NextRequest, { params }: { params: { id: string } }) {
     const token = extractToken(request.headers);
