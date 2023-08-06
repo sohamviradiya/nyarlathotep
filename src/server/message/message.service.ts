11f8d6d01c3b3be8b5978aa3b7a897f111422391
sohamviradiya <sohamviradiya2003@gmail.com>
2023-08-06 18:52:47 +0530

    resolve message deletion errors

--- src/server/message/message.service.ts
+++ src/server/message/message.service.ts
@@ -1,10 +1,11 @@
 import { castToContact } from "@/server/contact/contact.util";
-import {  ContactCollection,MessageCollection,} from "@/server/firebase/admin.init";
+import { ContactCollection, MessageCollection, } from "@/server/firebase/admin.init";
 import { Service_Response, STATUS_CODES } from "@/server/response/response.module";
 import { MESSAGE_DIRECTION, MESSAGE_STATUS, MESSAGE_STATUS_TYPE, Message } from "@/server/message/message.module";
 import { castToMessage, checkStatus } from "@/server/message/message.util";
 import { FieldValue, Timestamp } from "firebase-admin/firestore";
 import { verifyClientToken } from "../auth/auth.service";
+import { Forbidden } from "../response/response.util";
 
 export async function getMessage(message_id: string, token: string): Promise<Service_Response<null | { message: Message }>> {
     const auth_response = await verifyClientToken(token);
@@ -15,10 +16,7 @@ export async function getMessage(message_id: string, token: string): Promise<Ser
     const message = castToMessage(messageDoc);
     const contact = castToContact(await messageDoc.data()?.contact.get());
     if (contact.sender != auth_response.data.email && contact.receiver != auth_response.data.email) {
-        return {
-            code: STATUS_CODES.UNAUTHORIZED,
-            message: "Unauthorized",
-        };
+        return Forbidden({ message: `You are not authorized to view this message ${message_id}` });
     }
     return {
         code: STATUS_CODES.OK,
@@ -35,9 +33,9 @@ export async function addMessage(contact_id: string, content: string, token: str
         return auth_response as Service_Response<null>;
     const contactRef = ContactCollection.doc(contact_id);
     const contact = castToContact(await contactRef.get());
-    
+
     if (contact.sender === auth_response.data.email) {
-        const message_id = `${contact_id}.OUTGOING.${contact.messages.outgoing.length}`; 
+        const message_id = `${contact_id}.OUTGOING.${contact.messages.outgoing.length}`;
         await MessageCollection.doc(message_id).set({
             content,
             contact: contactRef,
@@ -66,7 +64,7 @@ export async function addMessage(contact_id: string, content: string, token: str
             status_changed: Timestamp.now(),
             direction: MESSAGE_DIRECTION.INCOMING,
         });
-        const messageRef =  MessageCollection.doc(message_id);
+        const messageRef = MessageCollection.doc(message_id);
         await contactRef.update({
             "messages.incoming": FieldValue.arrayUnion(messageRef)
         });
@@ -78,12 +76,8 @@ export async function addMessage(contact_id: string, content: string, token: str
             }
         };
     }
-    else {
-        return {
-            code: STATUS_CODES.UNAUTHORIZED,
-            message: `You are not authorized to add messages to ${contact_id}`,
-        };
-    }
+    else
+        return Forbidden({ message: `You are not authorized to add messages to ${contact_id}` });
 }
 
 export async function updateMessage(message_id: string, content: string, token: string): Promise<Service_Response<null | {
@@ -97,19 +91,12 @@ export async function updateMessage(message_id: string, content: string, token: 
     const auth_response = await verifyClientToken(token);
     if (!auth_response.data)
         return auth_response as Service_Response<null>;
-    if ((message.direction == MESSAGE_DIRECTION.OUTGOING && contact.sender !== auth_response.data.email) || (message.direction == MESSAGE_DIRECTION.INCOMING && contact.receiver !== auth_response.data.email)) {
-        return {
-            code: STATUS_CODES.UNAUTHORIZED,
-            message: `You are not authorized to update messages to ${contactRef}`,
-        };
-    }
+    if ((message.direction == MESSAGE_DIRECTION.OUTGOING && contact.sender !== auth_response.data.email) || (message.direction == MESSAGE_DIRECTION.INCOMING && contact.receiver !== auth_response.data.email))
+        return Forbidden({ message: `You are not authorized to update messages to ${contactRef.id}` });
 
-    if (message.status == MESSAGE_STATUS.READ || message.status == MESSAGE_STATUS.APPROVED || message.status == MESSAGE_STATUS.REJECTED) {
-        return {
-            code: STATUS_CODES.FORBIDDEN,
-            message: `Message ${message_id} is in ${message.status} state, cannot update`,
-        };
-    };
+    if (message.status == MESSAGE_STATUS.READ || message.status == MESSAGE_STATUS.APPROVED || message.status == MESSAGE_STATUS.REJECTED)
+        return Forbidden({ message: `Message ${message_id} is in ${message.status} state, cannot update` });
+
     await messageRef.update({
         content,
         status_changed: Timestamp.now(),
@@ -162,27 +149,20 @@ export async function deleteMessage(message_id: string, token: string): Promise<
     if (!auth_response.data)
         return auth_response as Service_Response<null>;
     console.log(contact);
-    if ((message.direction== MESSAGE_DIRECTION.OUTGOING && contact.sender !== auth_response.data.email) || (message.direction== MESSAGE_DIRECTION.INCOMING && contact.receiver !== auth_response.data.email)) {
-        return {
-            code: STATUS_CODES.UNAUTHORIZED,
-            message: `You are not authorized to update messages to ${contactRef}`,
-        };
-    }
+    if ((message.direction == MESSAGE_DIRECTION.OUTGOING && contact.sender !== auth_response.data.email) || (message.direction == MESSAGE_DIRECTION.INCOMING && contact.receiver !== auth_response.data.email))
+        return Forbidden({ message: `You are not authorized to update messages to ${contactRef}`, });
 
-    if (message.status == MESSAGE_STATUS.READ || message.status == MESSAGE_STATUS.APPROVED || message.status == MESSAGE_STATUS.REJECTED) {
-        return {
-            code: STATUS_CODES.FORBIDDEN,
-            message: `Message ${message_id} is in ${message.status} state, cannot delete`,
-        };
-    };
+    if (message.status == MESSAGE_STATUS.READ || message.status == MESSAGE_STATUS.APPROVED || message.status == MESSAGE_STATUS.REJECTED)
+        return Forbidden({ message: `Message ${message_id} is in ${message.status} state, cannot delete`, });
 
-    await contactRef.update({
-        messages: {
-            [message.direction.toLowerCase()]: {
-                [message.status.toLowerCase()]: FieldValue.arrayRemove(messageRef),
-            }
-        }
-    });
+    if (message.direction == MESSAGE_DIRECTION.OUTGOING)
+        await contactRef.update({
+            "messages.outgoing": FieldValue.arrayRemove(messageRef),
+        });
+    else
+        await contactRef.update({
+            "messages.incoming": FieldValue.arrayRemove(messageRef),
+        });
 
     await messageRef.delete();
     return {
