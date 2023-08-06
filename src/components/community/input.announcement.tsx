dd12e1e242dff4233f6768fa287f68e56364ccef
sohamviradiya <sohamviradiya2003@gmail.com>
2023-08-06 18:55:48 +0530

    add announcement input component

---
+++ src/components/community/input.announcement.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
+export default function AnnouncementInput({ id }: { id: string }) {
+    const [content, setContent] = useState("");
+    return (
+        <>
+            <input type="text" value={content} onChange={(e) => setContent(e.target.value)} />
+            <button onClick={() => sendAnnouncement(id, content)}>Send</button>
+        </>
+    );
+};
+
+async function sendAnnouncement(id: string, content: string) {
+    const response = await fetch(`/api/community/${id}/announce`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem('token')}`
+        },
+        body: JSON.stringify({ content }),
+    });
+    const data = await response.json();
+};
+
+
