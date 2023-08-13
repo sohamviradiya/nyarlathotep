f46f3caae3dc9c55065a658e42b212af92641834
sohamviradiya <sohamviradiya2003@gmail.com>
2023-08-13 11:52:08 +0530

    style footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,40 +1,71 @@
-import * as React from 'react';
-import { AppBar, Toolbar, Container, Typography } from '@mui/material';
-import { AuthActions } from '@/components/auth/auth-actions';
-import ThemeHydrator from './mui/theme';
-import Link from 'next/link';
+"use client";
+import { useEffect, useState } from 'react';
+import { Container, Toolbar, Typography } from '@mui/material';
+import ThemeHydrator from '@/components/mui/theme';
+import { CopyrightOutlined, GitHub } from '@mui/icons-material';
 
 function FooterComponent() {
     return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Link href="/">
-                    <Typography
-                        variant="h3"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            display:  'flex',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem'
-                        }}
-                    >
-                        Home
-                        </Typography>
-                    </Link>
-                    <AuthActions />
-                </Toolbar>
-            </Container>
-        </AppBar>
+        <Container sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: '1rem',
+            gap: '1rem',
+            width: '100%',
+        }} maxWidth="xl">
+
+            <Toolbar>
+                <CopyrightOutlined sx={{ fontSize: '2rem', margin: '0.5rem' }} />
+                <FooterTypoGraphy>
+                    2023, Soham Viradiya
+                </FooterTypoGraphy>
+            </Toolbar>
+            <FooterTypoGraphy>
+                <FooterDate />
+            </FooterTypoGraphy>
+            <FooterTypoGraphy>
+                <a href={"https://github.com/sohamviradiya/nyarlathotep"} target='_blank'> <GitHub sx={{ fontSize: '2rem' }} /></a>
+            </FooterTypoGraphy>
+        </Container>
     );
 };
 
 export default function Footer() {
     return (
         <ThemeHydrator>
-        <FooterComponent />
+            <FooterComponent />
         </ThemeHydrator>
+    );
+}
+
+function FooterTypoGraphy({ children }: { children: React.ReactNode }) {
+    return (
+        <Typography variant="h5"
+            noWrap
+            component="a"
+            sx={{
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+            }}
+        >
+            {children}
+        </Typography>
+    )
+};
+
+function FooterDate() {
+    const [date, setDate] = useState<Date>(new Date());
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+    return (
+        <>
+            {date.toUTCString()}
+        </>
     );
 }
