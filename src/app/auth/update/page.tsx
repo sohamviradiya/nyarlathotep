85e8dfe6c4b197f16db1f86c231e12306bc18e9d
sohamviradiya <sohamviradiya2003@gmail.com>
2023-08-13 19:32:40 +0530

    add update password page

--- src/app/auth/update/page.tsx
+++ src/app/auth/update/page.tsx
@@ -9,56 +9,33 @@ import ThemeHydrator from "@/components/mui/theme";
 
 const client_side_errors = ["Password must be at least 8 characters long", "Invalid Email"];
 
-function LoginComponent() {
-    const [user, setUser] = useState<{ email: string, password: string }>({
-        password: "",
-        email: "",
-    });
+function UpdateComponent() {
+    const [user, setUser] = useState<{ email: string, currentPassword: string, newPassword: string }>({ newPassword: "", email: "", currentPassword: "" });
     const [errors, setErrors] = useState<string[]>([]);
     const [showPassword, setShowPassword] = useState<boolean>(false);
     const [waiting, setWaiting] = useState<boolean>(false);
     const router = useRouter();
 
     useEffect(() => {
-        localStorage.removeItem("email");
+        const email = localStorage.getItem("email");
+        if (!email) router.push("/auth/log-in");
+        else
+            setUser({ currentPassword: "", newPassword: "", email: email });
         localStorage.removeItem("token");
-    }, []);
+    }, [router]);
 
     return (
         <Container fixed maxWidth="md" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "80vh" }}>
             <FormControl variant="filled" sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-                <TextField
-                    label="Email"
-                    variant="filled"
-                    type="email"
-                    onChange={(e) => {
-                        if (e.target.validity.typeMismatch) {
-                            if (!errors.includes("Invalid Email"))
-                                setErrors([...errors, "Invalid Email"]);
-                        }
-                        else {
-                            setErrors(errors.filter((error) => error !== "Invalid Email" && client_side_errors.includes(error)));
-                            setUser({ ...user, email: e.target.value });
-                        }
-                    }}
-                />
-                <InputLabel htmlFor="password" >Password</InputLabel>
-                <FilledInput
-                    id="password"
-                    type={showPassword ? 'text' : 'password'}
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={() => { setShowPassword(!showPassword) }}
-                                onMouseDown={(e) => { e.preventDefault(); }}
-                                edge="end"
-                            >
-                                {showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-
+                <TextField label="Email" variant="filled" type="email" disabled value={user.email} />
+                <InputLabel htmlFor="current-password" >Password</InputLabel>
+                <FilledInput id="current-password" type={showPassword ? 'text' : 'password'} endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton onClick={() => { setShowPassword(!showPassword) }} onMouseDown={(e) => { e.preventDefault(); }} edge="end">
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                }
                     onChange={
                         (e) => {
                             if (e.target.value.length < 8) {
@@ -66,12 +43,35 @@ function LoginComponent() {
                                     setErrors([...errors, "Password must be at least 8 characters long"]);
                             }
                             else {
-                                setUser({ ...user, password: e.target.value });
+                                setUser({ ...user, currentPassword: e.target.value });
                                 setErrors(errors.filter((error) => error !== "Password must be at least 8 characters long" && client_side_errors.includes(error)));
                             }
                         }
                     }
                 />
+
+                <InputLabel htmlFor="new-password" >New Password</InputLabel>
+                <FilledInput id="new-password" type={showPassword ? 'text' : 'password'} endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton onClick={() => { setShowPassword(!showPassword) }} onMouseDown={(e) => { e.preventDefault(); }} edge="end">
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                }
+                    onChange={
+                        (e) => {
+                            if (e.target.value.length < 8) {
+                                if (!errors.includes("Password must be at least 8 characters long"))
+                                    setErrors([...errors, "Password must be at least 8 characters long"]);
+                            }
+                            else {
+                                setUser({ ...user, newPassword: e.target.value });
+                                setErrors(errors.filter((error) => error !== "Password must be at least 8 characters long" && client_side_errors.includes(error)));
+                            }
+                        }
+                    }
+                />
+
                 <Button variant="contained" sx={{ background: "green" }} onClick={(e) => {
                     e.preventDefault();
                     submitForm(user, setErrors, setWaiting).then((email) => {
@@ -92,18 +92,20 @@ function LoginComponent() {
 
 async function submitForm(user: {
     email: string,
-    password: string,
+    currentPassword: string,
+    newPassword: string
 }, setErrors: (errors: string[]) => void, setWaiting: (waiting: boolean) => void) {
     setWaiting(true);
     const response = await fetch("/api/auth", {
-        method: "POST",
+        method: "PUT",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
             auth: {
                 email: user.email,
-                password: user.password,
+                currentPassword: user.currentPassword,
+                newPassword: user.newPassword,
             }
         }),
     });
@@ -128,10 +130,10 @@ export const metadata: Metadata = {
     description: "Log In Page for NyarlaThotep",
 };
 
-export default function Login() {
+export default function Update() {
     return (
         <ThemeHydrator>
-            <LoginComponent />
+            <UpdateComponent />
         </ThemeHydrator>
     )
 }
