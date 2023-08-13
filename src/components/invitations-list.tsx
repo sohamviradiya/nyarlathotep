5422b6dbe20141e2ce99c64372296a2f5aae5264
sohamviradiya <sohamviradiya2003@gmail.com>
2023-08-13 13:52:44 +0530

    style invitations list

--- src/components/invitations-list.tsx
+++ src/components/invitations-list.tsx
@@ -1,18 +1,16 @@
 import { useState, useEffect } from "react";
 import { APPEAL_STATUS, APPEAL_STATUS_TYPE, Appeal } from "@/server/appeal/appeal.module";
 import SkeletonBundle from "@/components/skeleton-bundle";
+import { Button, Card, CardActions, CardHeader, Container, List, ListItem, Typography } from "@mui/material";
 
 export default function InvitationList({ invitations }: { invitations: string[] }) {
-    return (<>
-        <ul>
-            {invitations.map((invitation) => (
-                <li key={invitation}>
-                    <Invitation invitation_id={invitation} />
-                </li>
-            ))}
-        </ul>
-    </>);
-}
+    return (<Container maxWidth="xl">
+        <Typography variant="h4" gutterBottom> Appeals: </Typography>
+        <List>
+            {invitations.map((invitation) => <ListItem key={invitation}> <Invitation invitation_id={invitation} /> </ListItem>)}
+        </List>
+    </Container>);
+};
 
 function Invitation({ invitation_id }: { invitation_id: string }) {
     const [invitation, setInvitation] = useState<Appeal>();
@@ -53,14 +51,13 @@ function Invitation({ invitation_id }: { invitation_id: string }) {
             });
         }
     }, [status, invitation_id]);
-    return ((invitation) ? (<>
-        <h4>{invitation.sender}</h4>
-        <p>{invitation.message}</p>
-        <h4>{invitation.type} to {invitation.receiver} </h4>
-        <h5>{invitation.status.toLocaleLowerCase()}</h5>
-        <h6>{new Date(invitation.status_changed).toLocaleTimeString()}</h6>
-        <InvitationActions status={status} setStatus={setStatus} />
-    </>) : (<SkeletonBundle size={1} />));
+    return ((invitation) ? (<Card variant="outlined">
+        <CardHeader title={`${invitation.type} from ${invitation.sender}`} subheader={`${new Date(invitation.status_changed).toLocaleString()}`} />
+        <Typography variant="body1" paragraph>  {invitation.message} </Typography>
+        <Typography variant="subtitle1" gutterBottom> {invitation.status}</Typography>
+        <CardActions disableSpacing> <InvitationActions status={status} setStatus={setStatus} /></CardActions>
+    </Card>
+    ) : (<SkeletonBundle size={1} />));
 };
 
 function InvitationActions({ status, setStatus }: { status: APPEAL_STATUS_TYPE, setStatus: (status: APPEAL_STATUS_TYPE) => void }) {
@@ -70,8 +67,8 @@ function InvitationActions({ status, setStatus }: { status: APPEAL_STATUS_TYPE, 
     else if (status === APPEAL_STATUS.UNDER_REVIEW) {
         return (
             <>
-                <button onClick={() => setStatus(APPEAL_STATUS.ACCEPTED)}>Accept</button>
-                <button onClick={() => setStatus(APPEAL_STATUS.REJECTED)}>Reject</button>
+                <Button variant="outlined" onClick={() => setStatus(APPEAL_STATUS.ACCEPTED)}>Accept</Button>
+                <Button variant="outlined" onClick={() => setStatus(APPEAL_STATUS.REJECTED)}>Reject</Button>
             </>
         );
     }
@@ -79,11 +76,7 @@ function InvitationActions({ status, setStatus }: { status: APPEAL_STATUS_TYPE, 
         return (<></>);
     }
     else if (status === APPEAL_STATUS.REJECTED) {
-        return (
-            <>
-                <button onClick={() => setStatus(APPEAL_STATUS.ACCEPTED)}>Accept</button>
-            </>
-        );
+        return (<Button variant="outlined" onClick={() => setStatus(APPEAL_STATUS.ACCEPTED)}>Accept</Button>);
     }
 };
 
