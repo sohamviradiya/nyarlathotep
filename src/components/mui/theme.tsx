8ed7618708a6b21ad2ca1ba3b9782c117df17cf1
sohamviradiya <sohamviradiya2003@gmail.com>
2023-08-13 11:50:15 +0530

    add styles

--- src/components/mui/theme.tsx
+++ src/components/mui/theme.tsx
@@ -1,23 +1,25 @@
-"use client";
 import { ThemeOptions, ThemeProvider, createTheme } from '@mui/material/styles';
-
+import { createContext } from 'react';
 const themeOptions: ThemeOptions = {
     palette: {
         mode: 'dark',
         primary: {
-            main: '#28038c',
+            main: '#4843ac',
             light: '#4206f5',
             dark: '#15073d',
         },
         secondary: {
             main: '#6400a2',
+            light: '#8c00d4',
+            dark: '#3c0070',
         },
-    },
-    typography: {
-        fontSize: 16,
-        fontWeightLight: 100,
-        fontWeightRegular: 300,
-        fontWeightMedium: 500,
+        background: {
+            default: '#000000',
+        },
+        text: {
+            primary: '#ffffff',
+            secondary: '#fffffff',
+        }
     },
 };
 
