bb79b504b7005e39a8cf25ab5f7858799950401d
sohamviradiya <sohamviradiya2003@gmail.com>
2023-08-24 14:14:19 +0530

    add google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
     title: 'Nyarlathotep',
     description: 'A Secure chat application',
+    verification: {
+        google: 'hgtdHl6GDpYmoYXqHGa4k5W7iJYWQOh5QZ8tw9oGiyQ',
+    }
 };
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
